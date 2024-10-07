3d0fb134edf32318ab973ceb26393b89c779954a
Luka Lopez <luka.lopez.j@gmail.com>
2024-10-07 19:52:19 +0200

    Ejercicio 3 de bucles corregido.

--- src/main/java/com/lukalopez/tema03/bucles/Ejercicio3.java
+++ src/main/java/com/lukalopez/tema03/bucles/Ejercicio3.java
@@ -2,18 +2,25 @@ package com.lukalopez.tema03.bucles;
 
 import java.util.Scanner;
 
+import static java.lang.Math.abs;
+
 public class Ejercicio3 {
     public static void main(String args[]) {
-        float suma, numero1, numero2;
+        float suma, num1, num2;
+        int cNeg;
         Scanner lector = new Scanner(System.in);
         System.out.println("En este programa se devolverá el producto de la multiplicación de dos números. A continuación proporciona el primer número:");
-        numero1 = Float.parseFloat(lector.nextLine());
+        num1 = Float.parseFloat(lector.nextLine());
         System.out.println("Introduce el segundo número: ");
-        numero2 = Float.parseFloat(lector.nextLine());
+        num2 = Float.parseFloat(lector.nextLine());
         lector.close();
-        suma=numero1;
-        for (float i=1;i<numero2;i++){
-            suma=suma+numero1;
+        suma=abs(num1);
+        for (float i=1;i<abs(num2);i++){
+            suma=suma+abs(num1);
+        }
+        if ((num1<0&&num2>0)||(num1>0&&num2<0)) {
+            cNeg = -1;
+            suma=suma*cNeg;
         }
         System.out.println("El resultado de la multiplicación es: " + suma);
     }
