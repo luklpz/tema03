d07d15acf956082f69f2fa4449de1581246ef2a5
luklpz <luka.lopez.j@gmail.com>
2024-10-15 15:48:36 +0200

    Ejercicio 3 corregido.

--- src/main/java/com/lukalopez/tema03/ANSI/Ejercicio3.java
+++ src/main/java/com/lukalopez/tema03/ANSI/Ejercicio3.java
@@ -2,6 +2,18 @@ package com.lukalopez.tema03.ANSI;
 
 public class Ejercicio3 {
     public static void main(String args[]) {
+        String cadena = "";
+        for (int i=1;i<10;i++){
+            cadena = cadena + i;
+            System.out.printf("%9s\n",cadena);
+        }
+    }
+}
+
+
+
+
+        /*
         for (int i=1;i<10;i++) {
             int esp = (9-i+1);
             System.out.printf("%"+esp+"s","");
@@ -13,3 +25,4 @@ public class Ejercicio3 {
         }
     }
 }
+*/
