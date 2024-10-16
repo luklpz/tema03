3c5c0945eed265511378f23ab4c7ce908929b96a
luklpz <luka.lopez.j@gmail.com>
2024-10-16 15:48:36 +0200

    Ejercicio 8 ANSI retocado.

--- src/main/java/com/lukalopez/tema03/ANSI/Ejercicio8.java
+++ src/main/java/com/lukalopez/tema03/ANSI/Ejercicio8.java
@@ -3,17 +3,19 @@ package com.lukalopez.tema03.ANSI;
 public class Ejercicio8 {
     public static void main(String args[]) {
         int b=0;
+        //Para repetir dos veces
         for (int cont=0;cont!=2;cont++) {
+            //Numero de veces que se crea la fila
             for (int fc = 0; fc < 8; fc++) {
+                //Numero de columnas que se generan
                 for (int bc = 0; bc < 8; bc++) {
-                    String cuadrante="\u001B["+b+";"+fc+";"+bc+"m";
-                    System.out.print(cuadrante);
-                    System.out.print(" "+b+";3"+fc+";4"+bc+" ");
+                    System.out.print("\u001B["+b+";3"+fc+";4"+bc+"m "+b+";3"+fc+";4"+bc+" ");
                 }
+                System.out.print("\u001B[0m");
                 System.out.println();
             }
             b=1;
         }
-        System.out.print("\u001B[0m");
+
     }
 }
