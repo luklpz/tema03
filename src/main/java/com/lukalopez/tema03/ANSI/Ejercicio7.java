02b70dd08b3efa3301a511214b920d17bfd6657e
luklpz <luka.lopez.j@gmail.com>
2024-10-16 16:18:50 +0200

    Ejercicio 7 corregido.

--- src/main/java/com/lukalopez/tema03/ANSI/Ejercicio7.java
+++ src/main/java/com/lukalopez/tema03/ANSI/Ejercicio7.java
@@ -3,15 +3,18 @@ package com.lukalopez.tema03.ANSI;
 public class Ejercicio7 {
     public static void main(String args[]) {
         String cadena = "";
-        String cadena2 = "";
+        String cadena2;
+        char position=0;
         for (int i = 1; i < 10; i++) {
             cadena = cadena + i;
             System.out.printf("%9s", cadena);
 
-            do {
-                cadena2 = i + cadena2;
-                System.out.printf("%s\n", cadena2);
-            } while (1!=1);
+            cadena2="";
+            for (int x=(i-1);x>-1;x--) {
+                position=cadena.charAt(x);
+                cadena2 = cadena2 + position;
+            }
+            System.out.printf("%s\n", cadena2);
         }
     }
 }
